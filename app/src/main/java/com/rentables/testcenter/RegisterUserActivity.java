c1a7bdd30c452e4a8a3aee62f9d7b3928847bb75
HertelDavid <herteldavid1@gmail.com>
2016-11-02 08:50:53 -0700

    Test Commit

---
+++ app/src/main/java/com/rentables/testcenter/RegisterUserActivity.java
@@ -0,0 +1,14 @@
+package com.rentables.testcenter;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class RegisterUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_register_user);
+    }
+}
