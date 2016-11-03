d6410d4b0566ec6ac95746bafc7898a05ca20e3c
HertelDavid <herteldavid1@gmail.com>
2016-11-03 10:48:56 -0700

    Added required fields

--- app/src/main/java/com/rentables/testcenter/MainActivity.java
+++ app/src/main/java/com/rentables/testcenter/MainActivity.java
@@ -10,8 +10,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
 import android.text.method.PasswordTransformationMethod;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnKeyListener;
 import android.widget.EditText;
 
 import java.net.URI;
@@ -27,7 +29,12 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.rentables_toolbar);
         setSupportActionBar(toolbar);
 
+        //Adding onKeyListener for password EditText
+        onKeyListenerForPassword();
+
         resetPasswordTypeface();
+
+        EditText userName = (EditText) findViewById(R.id.username_edit_text);
     }
 
     @Override
@@ -40,9 +47,23 @@ public class MainActivity extends AppCompatActivity {
 
     public void userLogin(View view){
 
+        EditText userName = (EditText) findViewById(R.id.username_edit_text);
+        EditText password = (EditText) findViewById(R.id.password_edit_text);
+
+        if(userName.getText().toString().trim().equals("")){
+
+            userName.setError("Username Required");
+        }
+
+        if(password.getText().toString().trim().equals("")){
+
+            password.setError("Password Required");
+        }
+
+        /*
         Intent intent = new Intent();
         intent.setClass(this, com.rentables.testcenter.HomeActivity.class);
-        startActivity(intent);
+        startActivity(intent);*/
     }
 
     public void forgotPassword(View view){
@@ -65,5 +86,25 @@ public class MainActivity extends AppCompatActivity {
         EditText password = (EditText) findViewById(R.id.password_edit_text);
         password.setTypeface(Typeface.DEFAULT);
         password.setTransformationMethod(new PasswordTransformationMethod());
+    }
+
+    public void onKeyListenerForPassword(){
+
+        final EditText passText = (EditText) findViewById(R.id.password_edit_text);
+
+        passText.setOnKeyListener(new OnKeyListener(){
+
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event){
+
+                if(event.getKeyCode() == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_UP){
+
+                    userLogin(passText);
+                    return true;
+                }
+
+                return false;
+            }
+        });
     }
 }
