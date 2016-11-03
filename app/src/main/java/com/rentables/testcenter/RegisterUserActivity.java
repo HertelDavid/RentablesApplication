73d9b086ec9143f321a8432acfe2f1b85b81c49e
HertelDavid <herteldavid1@gmail.com>
2016-11-02 19:05:58 -0700

    Added Register Activity

--- app/src/main/java/com/rentables/testcenter/RegisterUserActivity.java
+++ app/src/main/java/com/rentables/testcenter/RegisterUserActivity.java
@@ -1,7 +1,12 @@
 package com.rentables.testcenter;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.PasswordTransformationMethod;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
 
 public class RegisterUserActivity extends AppCompatActivity {
 
@@ -10,5 +15,36 @@ public class RegisterUserActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_register_user);
+
+        resetPasswordTypeface();
+        setKeyListeners();
+
+        //Set Focus to Email Edit Text
+        setFocusOfEditText(R.id.register_username);
+    }
+
+    public void resetPasswordTypeface(){
+
+        EditText password = (EditText) findViewById(R.id.register_password);
+        password.setTypeface(Typeface.DEFAULT);
+        password.setTransformationMethod(new PasswordTransformationMethod());
+    }
+
+    public void setKeyListeners(){
+
+
+    }
+
+    public void setFocusOfEditText(int viewId){
+
+        EditText viewToFocus = (EditText) findViewById(viewId);
+        viewToFocus.requestFocus();
+    }
+
+    public void setTextOfView(int viewId, String text){
+
+        System.out.println("Reached");
+        EditText viewToChange = (EditText) findViewById(viewId);
+        viewToChange.setText("");
     }
 }
