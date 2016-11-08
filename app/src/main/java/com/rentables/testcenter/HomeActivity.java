780b6b3a20ce4986ac10ae50f0b72bb901d60ac5
tylerhunter1 <tyler.hunter.194@my.csun.edu>
2016-11-08 08:40:22 -0800

    Navigation Bar

--- app/src/main/java/com/rentables/testcenter/HomeActivity.java
+++ app/src/main/java/com/rentables/testcenter/HomeActivity.java
@@ -1,7 +1,11 @@
 package com.rentables.testcenter;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +14,28 @@ public class HomeActivity extends AppCompatActivity {
 
         super.onCreate(savedInstance);
         setContentView(R.layout.activity_home);
+
+        Toolbar toolbarMain = (Toolbar) findViewById(R.id.rentables_toolbar);
+        setSupportActionBar(toolbarMain);
+
+        Toolbar toolbarNavigate = (Toolbar) findViewById(R.id.navigate_toolbar);
+        setSupportActionBar(toolbarNavigate);
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+
+        getMenuInflater().inflate(R.menu.overflow_menu, menu);
+
+        return true;
+    }
+
+    public void userLogin(View view){
+
+        Intent intent = new Intent();
+        intent.setClass(this, com.rentables.testcenter.NavigateActivity.class);
+        startActivity(intent);
+    }
+
 }
