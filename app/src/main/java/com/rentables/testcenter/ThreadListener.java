70f7a5d1838f0927924908accbf227aef1935d1d
HertelDavid <herteldavid1@gmail.com>
2016-11-12 12:15:52 -0800

    Added functionality on the Register user activity. Confirming passwords match. Checking to see if forms have been completed.

--- app/src/main/java/com/rentables/testcenter/ThreadListener.java
+++ app/src/main/java/com/rentables/testcenter/ThreadListener.java
@@ -2,5 +2,7 @@ package com.rentables.testcenter;
 
 public interface ThreadListener {
 
+    //When a thread is finished executing it will call this method below. Each object that
+    //is listening to that thread will have to have this method implemented.
     void notifyOfThreadCompletion(final NotifyingThread notifyThread);
 }
