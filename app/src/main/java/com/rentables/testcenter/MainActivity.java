4bf5bcd96923c1375342f7f00b9c856b9e0f5ced
HertelDavid <herteldavid1@gmail.com>
2016-11-12 12:15:52 -0800

    Added functionality on the Register user activity. Confirming passwords match. Checking to see if forms have been completed.

--- app/src/main/java/com/rentables/testcenter/MainActivity.java
+++ app/src/main/java/com/rentables/testcenter/MainActivity.java
@@ -9,13 +9,16 @@ import android.os.Bundle;
 import android.text.method.PasswordTransformationMethod;
 import android.view.KeyEvent;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnKeyListener;
 import android.widget.EditText;
 
+import dataobjects.User;
+
 public class MainActivity extends AppCompatActivity implements ThreadListener{
 
-    ServerGetUser get;
+    User firstUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +26,7 @@ public class MainActivity extends AppCompatActivity implements ThreadListener{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Setting up toolbar
+        //Setting up the toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.rentables_toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Rentables");
@@ -48,22 +51,32 @@ public class MainActivity extends AppCompatActivity implements ThreadListener{
     @Override
     public void notifyOfThreadCompletion(final NotifyingThread notifyingThread){
 
-        ServerGetUser thread = (ServerGetUser) notifyingThread;
-        thread.printProperties();
+        ThreadGetUser thread = (ThreadGetUser) notifyingThread;
+        firstUser.printProperties();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+
+        switch(item.getItemId()){
+
+            case R.id.overflow_settings_option:
+                System.out.println("Reached");
+                return true;
+            case R.id.overflow_account_option:
+                System.out.println("Reached");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     public void testServerConnection(){
 
-        //get = new ServerGetUser(1);
-        //get1 = new ServerGetUser(1);
-        //get2 = new ServerGetUser(22);
-        //get.addListener(this);
-        //get1.addListener(this);
-        //get2.addListener(this);
-        //new Thread(get).start();
-        //new Thread(get1).start();
-        //new Thread(get2).start();
-
+        /*firstUser = new User(32);
+        ThreadGetUser get = new ThreadGetUser(firstUser);
+        get.addListener(this);
+        new Thread(get).start();*/
     }
 
     public void userLogin(View view){
--- app/src/main/java/com/rentables/testcenter/RegisterUserActivity.java
+++ app/src/main/java/com/rentables/testcenter/RegisterUserActivity.java
@@ -4,16 +4,23 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.text.method.PasswordTransformationMethod;
 import android.view.KeyEvent;
+import android.view.View.OnKeyListener;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
 
 public class RegisterUserActivity extends AppCompatActivity {
 
+    private boolean passwordsMatch = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
+
+        System.out.println("Reached");
 
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_register_user);
@@ -22,11 +29,17 @@ public class RegisterUserActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.rentables_toolbar);
         setSupportActionBar(toolbar);
 
+        //Resetting password typeface for both password EditTexts.
         resetPasswordTypeface();
-        setKeyListeners();
+
+        //Adding listeners to the confirm password EditText
+        passwordChangeTextListener();
+        confirmPasswordListener();
+        confirmPasswordChangeTextListener();
 
         //Set Focus to Email Edit Text
         setFocusOfEditText(R.id.register_username);
+
     }
 
     @Override
@@ -38,19 +51,32 @@ public class RegisterUserActivity extends AppCompatActivity {
 
     public void registerUser(View view){
 
-        System.out.println(view);
+        /*
+        Before user can register make sure that the two passwords match. Also, make sure that
+        all fields in the form are filled in completely.
+         */
+
+        if(!isFormCompleted() || !passwordsMatch) {
+
+            //Checking to see if forms have been filled out completely and
+            //that the passwords match correctly.
+            return;
+        }else{
+
+            System.out.println("Hello");
+        }
     }
 
     public void resetPasswordTypeface(){
 
         EditText password = (EditText) findViewById(R.id.register_password);
+        EditText confirmPassword = (EditText) findViewById(R.id.register_password_confirm);
+
         password.setTypeface(Typeface.DEFAULT);
+        confirmPassword.setTypeface(Typeface.DEFAULT);
+
         password.setTransformationMethod(new PasswordTransformationMethod());
-    }
-
-    public void setKeyListeners(){
-
-
+        confirmPassword.setTransformationMethod((new PasswordTransformationMethod()));
     }
 
     public void setFocusOfEditText(int viewId){
@@ -59,10 +85,128 @@ public class RegisterUserActivity extends AppCompatActivity {
         viewToFocus.requestFocus();
     }
 
-    public void setTextOfView(int viewId, String text){
+    public void confirmPasswordListener(){
 
-        System.out.println("Reached");
-        EditText viewToChange = (EditText) findViewById(viewId);
-        viewToChange.setText("");
+        //A listener to check to see if the fields in the form are completely filled out.
+
+        final EditText confirmPassText = (EditText) findViewById(R.id.register_password_confirm);
+
+        confirmPassText.setOnKeyListener(new OnKeyListener(){
+
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+
+                if(event.getKeyCode() == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN){
+
+                   registerUser(confirmPassText);
+                }
+
+                return false;
+            }
+        });
+    }
+
+    public void passwordChangeTextListener(){
+
+        final EditText passText = (EditText) findViewById(R.id.register_password);
+
+        passText.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void afterTextChanged(Editable s) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                confirmPassword();
+            }
+        });
+    }
+
+    public void confirmPasswordChangeTextListener(){
+
+        //On text changed listener for the register_password_confirm EditText.
+        //Used to confirm that both password EditTexts match.
+
+        final EditText confirmPassText = (EditText) findViewById(R.id.register_password_confirm);
+
+        confirmPassText.addTextChangedListener(new TextWatcher(){
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void afterTextChanged(Editable s) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                confirmPassword();
+            }
+        });
+    }
+
+    public void confirmPassword(){
+
+        EditText passwordEditText = (EditText) findViewById(R.id.register_password);
+        EditText confirmPasswordEditText = (EditText) findViewById(R.id.register_password_confirm);
+
+        String password = passwordEditText.getText().toString();
+        String confirmPassword = confirmPasswordEditText.getText().toString();
+
+        if(confirmPassword.equals(password)){
+
+            passwordsMatch = true;
+            confirmPasswordEditText.setError(null);
+
+        }else{
+
+            passwordsMatch = false;
+            confirmPasswordEditText.setError("Passwords must match!");
+
+        }
+    }
+
+    public boolean isFormCompleted(){
+
+        EditText username = (EditText) findViewById(R.id.register_username);
+        EditText firstName = (EditText) findViewById(R.id.register_firstname);
+        EditText lastName = (EditText) findViewById(R.id.register_lastname);
+        EditText password = (EditText) findViewById(R.id.register_password);
+        EditText confirmPassword = (EditText) findViewById(R.id.register_password_confirm);
+
+        boolean formCompleted = true;
+
+        if(username.getText().toString().trim().equals("")){
+
+            formCompleted = false;
+            username.setError("Username Required");
+        }
+
+        if(firstName.getText().toString().trim().equals("")){
+
+            formCompleted = false;
+            firstName.setError("First Name Required");
+        }
+
+        if(lastName.getText().toString().trim().equals("")){
+
+            formCompleted = false;
+            lastName.setError("Last Name Required");
+        }
+
+        if(password.getText().toString().trim().equals("")){
+
+            formCompleted = false;
+            password.setError("Password Required");
+        }
+
+        if(confirmPassword.getText().toString().trim().equals("")){
+
+            formCompleted = false;
+        }
+
+        return formCompleted;
     }
 }
